faea0c5ad8ee4b71a3e0a5888da9b7b2dc4f9dc6
Stacy Jung <jeeannie@gmail.com>
2017-12-02 12:10:54 -0800

    asdf

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World from COMP 3916!'
+    return 'Hello, World from COMP 3916! howdy howdy'
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
